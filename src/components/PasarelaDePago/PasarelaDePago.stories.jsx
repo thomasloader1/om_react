2abe169f5219d9a95c5b523f326c67eb0a43a9d7
Gomez Tomas Gonzalo <tomasgomez@oceano.com.ar>
2022-08-16 23:53:25 -0300

    [ADD] PasarelaCobros Components, StepPasarela incompleto

--- src/components/PasarelaDePago/PasarelaDePago.stories.jsx
+++ src/components/PasarelaDePago/PasarelaDePago.stories.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Card from './index';
+import PasarelaDePago from './index';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'components/Card',
-  component: Card,
+  title: 'layouts/PasarelaDePago',
+  component: PasarelaDePago,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' }
@@ -14,21 +14,13 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 function Template(args) {
-  return <Card {...args} />;
+  return <PasarelaDePago {...args} />;
 }
 
-export const Primary = Template.bind({});
+export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+Default.args = {
   title: 'Actualización en politraumatismos y emergencias',
   size: '20rem',
   backgroundColor: '#FFF'
-};
-
-export const PasarelaDePagos = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-PasarelaDePagos.args = {
-  title: 'Actualización',
-  size: '20rem',
-  backgroundColor: 'red'
 };
---
+++ src/components/PasarelaDePago/index.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+// import PropTypes from 'prop-types';
+import '../../scss/pasarela-de-cobros.scss';
+
+/**
+ * Primary UI component for user interaction
+ */
+function PasarelaDePago() {
+
+  return (
+<p>asd</p>
+  );
+}
+
+PasarelaDePago.propTypes = {
+
+};
+
+PasarelaDePago.defaultProps = {
+  backgroundColor: '#FFF',
+  size: '20rem',
+};
+
+export default PasarelaDePago;
