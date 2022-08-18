f8647535e05faf37d89578a93cf2866b24a93aec
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2022-08-18 11:47:37 -0300

    [ADD] Layout home

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  BrowserRouter
+} from 'react-router-dom';
 import App from './components/App/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(process.env.REACT_APP_OCEANO_WP_API);
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
