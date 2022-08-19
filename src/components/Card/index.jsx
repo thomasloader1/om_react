c05c6e0c5adb43d121ead1bb095a6ae77f66d226
LautaroOceano <93997681+LautaroOceano@users.noreply.github.com>
2022-08-19 15:09:27 -0300

    First Commit
    Bulma React - ADDED
    React Icons - ADDED
    
    Component Button - ADDED

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,37 +1,41 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Card.scss';
 
 /**
  * Primary UI component for user interaction
  */
 function Card({ backgroundColor, size, title }) {
-
   return (
-<div className='card' style={{width: size, backgroundColor }}>
-
-    <div className="card-image">
+    <div className="card" style={{ width: size, backgroundColor }}>
+      <div className="card-image">
         <figure className="image is-4by3">
-            <img src="https://ar.oceanomedicina.tech/wp-content/uploads/2020/02/fotosecundaria-7.webp" alt=''/>
+          <img
+            src="https://ar.oceanomedicina.tech/wp-content/uploads/2020/02/fotosecundaria-7.webp"
+            alt=""
+          />
         </figure>
-    </div>
+      </div>
 
-    <div className="card-content">
-            <span className="tag is-link">Link</span>
+      <div className="card-content">
+        <span className="tag is-link">Link</span>
         <div className="content">
-            <p className='title is-4'>{title}</p>
+          <p className="title is-4">{title}</p>
         </div>
-    </div>
+      </div>
 
-    <div className="card-info-footer">
+      <div className="card-info-footer">
         <p className="card-footer-item">
-                <span>12 cuotas de $25</span>
+          <span>12 cuotas de $25</span>
         </p>
-        <button type='button' className="button is-danger is-fullwidth card-footer-item">Descubrir</button>
+        <button
+          type="button"
+          className="button is-danger is-fullwidth card-footer-item"
+        >
+          Descubrir
+        </button>
+      </div>
     </div>
-    
-</div>
   );
 }
 
@@ -47,12 +51,12 @@ Card.propTypes = {
   /**
    * Button contents
    */
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 Card.defaultProps = {
   backgroundColor: '#FFF',
-  size: '20rem',
+  size: '20rem'
 };
 
 export default Card;
