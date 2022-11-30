5025e82566fba59665437b466d01e66989145fe0
LautaroOceano <93997681+LautaroOceano@users.noreply.github.com>
2022-11-30 11:50:22 -0300

    styles structures & visual fix

--- src/components/PasarelaCobros/RadioButton/ButtonField.jsx
+++ src/components/PasarelaCobros/RadioButton/ButtonField.jsx
@@ -1,16 +1,16 @@
 import { useField } from 'formik';
-import React from 'react'
+import React from 'react';
 import { Block, Form, Notification } from 'react-bulma-components';
 import { Image } from 'semantic-ui-react';
 import IMAGES from '../../../img/pasarelaCobros/share';
 import SelectQuote from '../SelectQuote';
 
 const ButtonField = ({ img, showText, idElement, className, ...props }) => {
-  const [field, meta] = useField(props)
-  const { value } = props
+  const [field, meta] = useField(props);
+  const { value } = props;
 
-  const imageIcon = img && <Image src={IMAGES[img]} alt={value} size='small' />
-  const labelOfElement = showText ? value : ''
+  const imageIcon = img && <Image src={IMAGES[img]} alt={value} size="small" />;
+  const labelOfElement = showText ? value : '';
 
   return (
     <>
@@ -30,33 +30,31 @@ const ButtonField = ({ img, showText, idElement, className, ...props }) => {
         </Form.Control>
       </Form.Field>
 
-      {
-        meta.value === "Suscripción" ?
-          <SelectQuote selectName={'Seleccione la cantidad de coutas'} options={[1, 3, 6, 9, 12, 18]} />
-          : null
-      }
+      {meta.value === 'Suscripción' ? (
+        <SelectQuote
+          selectName={'Seleccione la cantidad de coutas'}
+          options={[1, 3, 6, 9, 12, 18]}
+        />
+      ) : null}
 
-      {
-        meta.errors && (
-          <p className="help is-danger">{meta.error}</p>
-        )
-      }
+      {meta.errors && <p className="help is-danger">{meta.error}</p>}
 
-      {
-        meta.value === "Tradicional" || meta.value === "Suscripción" ? (
-          <Block className='field_info'>
-            <Notification color="info">
-              <strong> {meta.value} </strong>
+      {meta.value === 'Tradicional' || meta.value === 'Suscripción' ? (
+        <Block className="field_info">
+          <Notification color="info" light="true">
+            <strong> {meta.value} </strong>
 
-              {meta.value === "Tradicional" ? " le permite hacer la operacion habitual de compra" : null}
-              {meta.value === "Suscripción" ? " le permite hacer la operacion con las cuotas sin interes!" : null}
-
-            </Notification>
-          </Block>
-        ) : null
-      }
+            {meta.value === 'Tradicional'
+              ? ' le permite hacer la operacion habitual de compra'
+              : null}
+            {meta.value === 'Suscripción'
+              ? ' le permite hacer la operacion con las cuotas sin interes!'
+              : null}
+          </Notification>
+        </Block>
+      ) : null}
     </>
-  )
-}
+  );
+};
 
-export default ButtonField
+export default ButtonField;
