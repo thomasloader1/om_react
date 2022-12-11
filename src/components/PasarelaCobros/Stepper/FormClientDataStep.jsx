d836c1f72e305ea787673f6fd204c8255102603f
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2022-12-11 14:49:12 -0300

    Modificando como mostrar errores del formulario

--- src/components/PasarelaCobros/Stepper/FormClientDataStep.jsx
+++ src/components/PasarelaCobros/Stepper/FormClientDataStep.jsx
@@ -1,7 +1,5 @@
 import React, { useContext } from 'react';
-import InputField from '../InputField/InputField';
 import { AppContext } from '../Provider/StateProvider';
-import ButtonField from '../RadioButton/ButtonField';
 import ResumeTicket from '../ResumeTicket';
 import { FormStep } from './MultiStep';
 
@@ -32,40 +30,6 @@ function FormClientDataStep() {
           <ResumeTicket contractId={formikValues.contractId} />
         </div>
       </FormStep>
-     /*  <FormStep
-        stepNumber={4}
-        stepName='Complete los datos del cliente'
-      >
-        <div id='grid-client_data'>
-          <div className="suscri_type">
-            
-            {clientFormRadioField.map((input) => (
-              input.options.map((option, i) => (
-                <ButtonField
-                  showText={true}
-                  className="button"
-                  key={input.idElement}
-                  value={option}
-                  idElement={`${input.idElement}-${i}`}
-                  name="type"
-                />
-              ))
-            ))}
-
-          </div>
-
-          {clientFormWithoutOptions.map((input) => (
-            <InputField
-              key={input.idElement}
-              name={input.idElement}
-              id={input.idElement}
-              placeholder={input.placeholder}
-              label={input.label}
-            />
-          ))}
-
-        </div>
-      </FormStep> */
     );
   }
 
