15ccfe0e90e9d0033f030861880f7dd1aa5664b9
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-12 15:34:58 -0300

    Visualizar los atributos dentro de formikValues

--- src/components/PasarelaCobros/App/index.jsx
+++ src/components/PasarelaCobros/App/index.jsx
@@ -13,43 +13,28 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import useStripeEnv from '../Hooks/useStripeEnv';
 
-const { REACT_APP_OCEANO_URL, REACT_APP_OCEANO_GENERATECHECKOUTPRO, NODE_ENV } =
-  process.env;
+const { REACT_APP_OCEANO_URL, REACT_APP_OCEANO_GENERATECHECKOUTPRO, NODE_ENV } = process.env;
 
 function PasarelaApp() {
-  const {
-    formikValues,
-    setFormikValues,
-    checkoutLink,
-    setCheckoutLink,
-    appRef,
-    userInfo
-  } = useContext(AppContext);
+  const { formikValues, setFormikValues, checkoutLink, setCheckoutLink, appRef, userInfo } =
+    useContext(AppContext);
   const [stepNumber, setStepNumber] = useState(0);
   const { stripePromise } = useStripeEnv();
 
   const validationSchemaFinalStep = Yup.object({
     fullName: Yup.string()
       .required('Ingrese nombre que figura en la tarjeta')
-      .matches(
-        /^[a-zA-Z]+\s+[a-zA-Z]+(?:\s+[a-zA-Z]+)?$/i,
-        'El campo debe contener solo letras'
-      ),
+      .matches(/^[a-zA-Z]+\s+[a-zA-Z]+(?:\s+[a-zA-Z]+)?$/i, 'El campo debe contener solo letras'),
     phone: Yup.string()
       .required('Ingrese un numero de telefono')
       .matches(/^[0-9]+$/i, 'El campo debe contener solo numeros'),
     address: Yup.string()
       .required('Ingrese calle y numero del titual de la tarjeta')
-      .matches(
-        /^.*[a-zA-Z]+\s[a-zA-Z]+\s[0-9]+.*$/i,
-        'El formato de la direccion es invalido'
-      ),
+      .matches(/^.*[a-zA-Z]+\s[a-zA-Z]+\s[0-9]+.*$/i, 'El formato de la direccion es invalido'),
     dni: Yup.string()
       .required('Ingrese el numero de tu documento de identidad')
       .matches(/^[0-9]+$/i, 'El campo debe contener solo numeros'),
-    email: Yup.string()
-      .email('Ingrese un email valido')
-      .required('El email es requerido')
+    email: Yup.string().email('Ingrese un email valido').required('El email es requerido'),
   });
 
   useEffect(() => {
@@ -64,8 +49,8 @@ function PasarelaApp() {
       const type = formikValues.mod.toLowerCase().substring(0, 4);
       const requestConfig = {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
       };
 
       body.append('months', 0);
@@ -91,17 +76,17 @@ function PasarelaApp() {
     <div ref={appRef}>
       <Header />
       <Elements stripe={stripePromise}>
-        <section className="container is-max-widescreen">
-          <div className="pasarela columns mx-auto">
+        <section className='container is-max-widescreen'>
+          <div className='pasarela columns mx-auto'>
             <MultiStep
               stepStateNumber={{ stepNumber, setStepNumber }}
-              className="pasarela-1 column seleccion-pais"
+              className='pasarela-1 column seleccion-pais'
               initialValues={{
                 fullName: '',
                 phone: '',
                 address: '',
                 dni: '',
-                email: ''
+                email: '',
               }}
               onSubmit={handleSubmitByStepTwo}
             >
@@ -109,31 +94,29 @@ function PasarelaApp() {
                 onSubmit={(values) => {
                   setFormikValues((prevFormikValues) => ({
                     ...prevFormikValues,
-                    ...values
+                    ...values,
                   }));
                 }}
                 validationSchema={Yup.object({
-                  country: Yup.string().required('El pais es requerido')
+                  country: Yup.string().required('El pais es requerido'),
                 })}
               />
               <SelectPaymentMethodStep
                 onSubmit={(values) => {
                   setFormikValues((prevFormikValues) => ({
                     ...prevFormikValues,
-                    ...values
+                    ...values,
                   }));
                 }}
                 validationSchema={Yup.object({
-                  payment_method: Yup.string().required(
-                    'El metodo de pago es requerido'
-                  )
+                  payment_method: Yup.string().required('El metodo de pago es requerido'),
                 })}
               />
               <SelectPaymentModeStep
                 onSubmit={(values) => {
                   setFormikValues((prevFormikValues) => ({
                     ...prevFormikValues,
-                    ...values
+                    ...values,
                   }));
                 }}
                 validationSchema={Yup.object({
@@ -142,19 +125,19 @@ function PasarelaApp() {
                   quotes: Yup.string().when('mod', {
                     is: (val) => !(val && val.includes('Tradicional')),
                     then: Yup.string().required('Especifique las cuotas'),
-                    otherwise: null
-                  })
+                    otherwise: null,
+                  }),
                 })}
               />
               <FormClientDataStep
                 onSubmit={(values) => {
                   setFormikValues((prevFormikValues) => ({
                     ...prevFormikValues,
-                    ...values
+                    ...values,
                   }));
                 }}
                 validationSchema={Yup.object({
-                  checkContract: Yup.string().required('El campo es requerido')
+                  checkContract: Yup.string().required('El campo es requerido'),
                 })}
               />
 
@@ -164,6 +147,7 @@ function PasarelaApp() {
               />
             </MultiStep>
           </div>
+          <pre>{JSON.stringify(formikValues, null, 2)}</pre>
         </section>
       </Elements>
     </div>
