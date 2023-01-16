227d8f736662329008b67265f454ad217a8f6711
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-16 16:54:23 -0300

    Hook ahora tiene la opcion de lanzar alertas grandes

--- src/components/PasarelaCobros/Hooks/useSwal.jsx
+++ src/components/PasarelaCobros/Hooks/useSwal.jsx
@@ -5,14 +5,30 @@ import withReactContent from 'sweetalert2-react-content';
 
 const toast = withReactContent(Swal);
 
-export function fireToast(title, icon = 'error') {
+export function fireToast(title, icon = 'error', timer = 3000) {
   toast.fire({
     title: <p>{title}</p>,
     toast: true,
     icon,
     position: 'bottom-end',
     showConfirmButton: false,
-    timer: 3000,
+    timer,
+    timerProgressBar: true,
+    didOpen: (alert) => {
+      alert.addEventListener('mouseenter', Swal.stopTimer);
+      alert.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
+}
+
+export function fireAlert(title, icon = 'error', timer = 3000) {
+  toast.fire({
+    title: <p>{title}</p>,
+    toast: false,
+    icon,
+    position: 'center-center',
+    showConfirmButton: false,
+    timer,
     timerProgressBar: true,
     didOpen: (alert) => {
       alert.addEventListener('mouseenter', Swal.stopTimer);
