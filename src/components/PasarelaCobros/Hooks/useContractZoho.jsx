72fab753478216eca0d4683dd479e458a5777487
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-16 11:40:28 -0300

    Agrego enpoint para usar test

--- src/components/PasarelaCobros/Hooks/useContractZoho.jsx
+++ src/components/PasarelaCobros/Hooks/useContractZoho.jsx
@@ -1,36 +1,43 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-const { REACT_APP_OCEANO_URL, REACT_APP_OCEANO_OBTAINDATA, NODE_ENV } = process.env
+const {
+  REACT_APP_OCEANO_URL,
+  REACT_APP_OCEANO_OBTAINDATA,
+  REACT_APP_OCEANO_OBTAINDATA_TEST,
+  NODE_ENV,
+} = process.env;
 
 export const useContractZoho = (contractId) => {
-  const [data, setData] = useState({})
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const body = new FormData();
-  body.append('key', '9j9fj0Do204==3fja134')
-  body.append('id', contractId)
-  const URL = NODE_ENV === "production" ? (`${REACT_APP_OCEANO_URL}${REACT_APP_OCEANO_OBTAINDATA}`) : REACT_APP_OCEANO_OBTAINDATA ;
+  body.append('key', '9j9fj0Do204==3fja134');
+  body.append('id', contractId);
+  const URL =
+    NODE_ENV === 'production'
+      ? `${REACT_APP_OCEANO_URL}${REACT_APP_OCEANO_OBTAINDATA}`
+      : REACT_APP_OCEANO_OBTAINDATA_TEST;
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.post(URL, body, {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-    //  // console.log({ response })
-        
-      setData(response.data)
-      setLoading(response.data.lenght > 0)
-       
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      //  // console.log({ response })
+
+      setData(response.data);
+      setLoading(response.data.lenght > 0);
+
       // // console.log({ response, data })
+    };
 
-    }
+    fetchData();
+  }, []);
 
-    fetchData()
-  }, [])
-
-  return { data, loading, error }
-}
+  return { data, loading, error };
+};
