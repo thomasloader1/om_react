4b18f5633554a9ad7cea6c3672463a244fd2716f
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-20 13:49:01 -0300

    Agrego el boton de guardado en los controles del form

--- src/components/VentaPresencial/StepControl/FormNavigation.jsx
+++ src/components/VentaPresencial/StepControl/FormNavigation.jsx
@@ -1,34 +1,61 @@
-import { useFormikContext } from 'formik';
 import axios from 'axios';
-import React, { useContext, useState } from 'react';
+import { useFormikContext } from 'formik';
+import React from 'react';
+import { useContext } from 'react';
 import { Button } from 'react-bulma-components';
-import { getAllISOCodes } from 'iso-country-currency';
-import { AppContext } from '../../PasarelaCobros/Provider/StateProvider'; 
+import { useParams } from 'react-router';
+import { AppContext } from '../../PasarelaCobros/Provider/StateProvider';
 
-const { REACT_APP_OCEANO_URL,REACT_APP_OCEANO_STRIPESUBSCRIPTION,REACT_APP_OCEANO_STRIPESUBSCRIPTION_LOCAL, NODE_ENV } = process.env
+const FormNavigation = ({ hasPrevious, isLastStep, onBackClick }) => {
+  const { values, ...formik } = useFormikContext();
+  const { stepNumberGlobal } = useContext(AppContext);
+  const { id } = useParams();
+  const { cardComplete, savingProgress } = values;
+  const disabledButton = isLastStep === !cardComplete || savingProgress;
+  const disabledSavingProgress = !formik.dirty;
+  /* console.log({ formik }); */
+  const handleSaveProgress = async () => {
+    formik.setFieldValue('savingProgress', true);
+    const response = await axios.put(`/api/progress/${id}`, {
+      step_number: stepNumberGlobal,
+      ...values,
+    });
 
+    console.log({ response });
+    formik.setFieldValue('savingProgress', false);
+  };
 
-const FormNavigation = (props) => {
-  const [fetching, setFetching] = useState(false);
-  const formik = useFormikContext();
-  const URL = NODE_ENV === "production" ? (`${REACT_APP_OCEANO_URL}${REACT_APP_OCEANO_STRIPESUBSCRIPTION}`) : REACT_APP_OCEANO_STRIPESUBSCRIPTION_LOCAL;
-  
-  const disabledButton = props.isLastStep === !formik.values.cardComplete
-
-  const [openModal, setOpenModal] = useState(null)
-  const { formikValues, stripeRequest, setStripeRequest, userInfo } = useContext(AppContext)
-  const { country, quotes, amount, sale, contact, products } = formikValues
-
-console.log({props})
-  
   return (
-    <div className='controls'>
-      {props.hasPrevious && (
-        <Button className="flex-grow-1 is-primary is-normal is-fullwidth" type='button' onClick={props.onBackClick}>Volver</Button>
+    <div className="controls">
+      {hasPrevious && (
+        <Button
+          className="flex-grow-1 is-primary is-normal is-fullwidth"
+          type="button"
+          onClick={onBackClick}
+          disabled={disabledButton}
+        >
+          Volver
+        </Button>
       )}
-      
-      {!props.isLastStep && (
-        <Button className={`flex-grow-1 is-primary is-normal is-fullwidth`} disabled={disabledButton} type='submit'>{props.isLastStep ? 'Pagar' : 'Siguiente'}</Button>
+
+      <Button
+        className={`flex-grow-1 is-success is-normal is-fullwidth ${
+          disabledButton ? 'is-loading is-hover' : 'is-outlined'
+        }`}
+        type="button"
+        onClick={handleSaveProgress}
+        disabled={disabledButton || disabledSavingProgress}
+      >
+        Guardar
+      </Button>
+      {!isLastStep && (
+        <Button
+          className={`flex-grow-1 is-primary is-normal is-fullwidth`}
+          disabled={disabledButton}
+          type="submit"
+        >
+          {isLastStep ? 'Pagar' : 'Siguiente'}
+        </Button>
       )}
     </div>
   );
