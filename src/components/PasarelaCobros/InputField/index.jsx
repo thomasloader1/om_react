242bcc7e9d7f3a44edbeaf16d8d7bf2495ae9c20
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-20 13:46:01 -0300

    Determino por name si devuleve el componente de telefono

--- src/components/PasarelaCobros/InputField/index.jsx
+++ src/components/PasarelaCobros/InputField/index.jsx
@@ -1,24 +1,44 @@
-import { useField } from 'formik'
-import React from 'react'
-
+import { useField, useFormikContext } from 'formik';
+import React from 'react';
+import IntlTelInput from 'react-intl-tel-input';
+import 'react-intl-tel-input/dist/main.css';
 const InputField = ({ label, ...props }) => {
-    const [field, meta] = useField(props)
+  const [field, meta] = useField(props);
+  const { setFieldTouched, setFieldValue } = useFormikContext();
 
-    return (
-        <div className="field">
-            <label htmlFor={props.id} className="label">
-                {label}
-            </label>
-            <div className="control">
-                <input
-                    className={meta.error ? 'input is-danger' : 'input'}
-                    {...field}
-                    {...props}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="field">
+      <label htmlFor={props.id} className="label">
+        {label}
+      </label>
+      <div className="control">
+        {props.id && props.name !== 'telephone' ? (
+          <input
+            className={meta.error ? 'input is-danger' : 'input'}
+            {...field}
+            {...props}
+          />
+        ) : (
+          <IntlTelInput
+            defaultCountry="ar"
+            containerClassName="intl-tel-input"
+            inputClassName={meta.error ? 'input is-danger' : 'input'}
+            type="tel"
+            defaultValue={field.value}
+            fieldId={field.id}
+            fieldName={field.name}
+            onPhoneNumberBlur={() => {
+              setFieldTouched(field.name, true);
+            }}
+            onPhoneNumberChange={(...args) => {
+              const [_, phone, countryObject] = args;
+              setFieldValue(field.name, `${countryObject.dialCode}${phone}`);
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default InputField
+export default InputField;
