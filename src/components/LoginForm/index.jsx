718b4f3aed04e611e12aa46dc57cf9ccb78ecddd
flidas-oceano <talk.tomas@hotmail.com>
2023-04-12 10:06:00 -0300

    login

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -5,47 +5,46 @@ import { useState } from 'react';
 import { AppContext } from '../PasarelaCobros/Provider/StateProvider';
 import { useNavigate } from 'react-router';
 import useToken from '../VentaPresencial/Hook/useToken';
+import { useSwal } from '../VentaPresencial/Hook/useSwal';
 
 function LoginForm({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
-  const { setToken } = useToken()
+  const { setToken } = useToken();
+  const { modalAlert } = useSwal();
   const { setIsAuthenticated, setTokenLogin } = useContext(AppContext);
-
-  const handleLogin = async (username, password) => {
-
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // TODO: Validar las credenciales y llamar a la función onLogin si son correctas
-    // event.preventDefault();
-    // const config = {
-    //   headers: { Authorization: `Bearer ${token}` }
-    // };
+
     try {
       const bodyParameters = {
-        email: username, password, remember_me: true
+        email: username,
+        password,
+        remember_me: true,
       };
-      const response = await axios.post('/api/login', bodyParameters,/*config*/);
-      console.log("Respuesta con token del login: ", response);
+      const response = await axios.post(
+        '/api/login',
+        bodyParameters /*config*/
+      );
+
       setIsAuthenticated(true);
 
       setToken(response.data.access_token);
 
       setTokenLogin(`Bearer ${response.data.access_token}`);
-      navigate("/ventapresencial/")
+      navigate('/ventapresencial/');
     } catch (error) {
-      console.log("Error en login de axios: ", error);
+      console.log('Error en login de axios: ', error);
 
       if (error.response && error.response.data) {
-        alert(error.response.data.messagge);
+        modalAlert(error.response.data.messagge, 'error');
         localStorage.setItem('tokenLogin', ``);
-        console.log("mensaje del server: ", error.response.data.messagge);
+        console.log('mensaje del server: ', error.response.data.messagge);
       } else {
-        alert('Error al intentar iniciar sesión');
+        modalAlert('Error al intentar iniciar sesión', 'error');
       }
     }
   };
@@ -61,15 +60,32 @@ function LoginForm({ onLogin }) {
             <form action="" className="box" onSubmit={handleSubmit}>
               <h2 className="title is-4">Login Venta Presencial</h2>
               <div className="field">
-                <label htmlFor="username" className="label">Usuario:</label>
+                <label htmlFor="username" className="label">
+                  Usuario:
+                </label>
                 <div className="control">
-                  <input id='username' type="text" value={username} className="input" onChange={(e) => setUsername(e.target.value)} required />
+                  <input
+                    id="username"
+                    type="text"
+                    value={username}
+                    className="input"
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
                 </div>
               </div>
               <div className="field">
-                <label htmlFor='' className="label">Contraseña</label>
+                <label htmlFor="" className="label">
+                  Contraseña
+                </label>
                 <div className="control">
-                  <input type="password" className="input" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                  <input
+                    type="password"
+                    className="input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
                 </div>
                 {/* <div className="control has-icons-left">
                   <input type="password" className="input" value={password} onChange={(e) => setPassword(e.target.value)} required/>
@@ -85,9 +101,7 @@ function LoginForm({ onLogin }) {
                 </label>
               </div> */}
               <div className="field">
-                <button className="button is-success">
-                  Login
-                </button>
+                <button className="button is-success">Login</button>
               </div>
             </form>
           </div>
