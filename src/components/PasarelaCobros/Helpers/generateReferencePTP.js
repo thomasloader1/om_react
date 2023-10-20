0f7b75a8a80d261126be67635bbb2f7ca68aa283
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-10-20 16:11:21 -0300

    Error de ref

--- src/components/PasarelaCobros/Helpers/generateReferencePTP.js
+++ src/components/PasarelaCobros/Helpers/generateReferencePTP.js
@@ -1,5 +1,5 @@
 export function generateReferencePTP(suffix) {
   const isProduction = process.env.NODE_ENV === 'production';
 
-  return isProduction ? `T_${suffix}` : suffix;
+  return !isProduction ? `T_${suffix}` : suffix;
 }
