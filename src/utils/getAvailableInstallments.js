d1cdac56be7aeb2c2e12de87a88e31c9d262b3e7
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-11-28 12:57:19 -0300

    Cambios sobre cuotas y configuracion

--- src/utils/getAvailableInstallments.js
+++ src/utils/getAvailableInstallments.js
@@ -1,10 +1,10 @@
 export const getAvailableInstallments = (countryCode) => {
     switch (countryCode) {
         case 'ch': //Chile
-            return [8, 6, 3, 1];
+            return [8, 6, 3];
         case 'ar':
-            return [1, 3, 6, 9, 12, 18, 24];
+            return [ 3, 6, 9, 12, 18, 24];
         default:
-            return [12, 9, 6, 3, 1];
+            return [12, 9, 6, 3];
     }
 };
